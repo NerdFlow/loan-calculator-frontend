1adaff2bc538fa91505c49750876db703e7693ba
SamarAli <62724986+Samar-Ali666@users.noreply.github.com>
2024-10-23 16:17:09 +0500

    store created

--- src/lib/slices/apiSlice.ts
+++ src/lib/slices/apiSlice.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // setting the base url domain for apis
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://jsonplaceholder.typicode.com",
+  baseUrl: "https://ccs.nerdflow.tech/api",
 });
 // creating the base api slice
 export const apiSlice = createApi({
